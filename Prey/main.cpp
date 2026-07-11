bea7a858b816aa1c1e4478b6c5e35906574fc814
Ahmad Abdullah Takie <20254103081@cse.bubt.edu.bd>
2026-07-11 18:57:15 +0000

    Added profile system and login and welcome screen

--- Prey/main.cpp
+++ Prey/main.cpp
@@ -1,6 +1,7 @@
-#include <iostream>
-#include <termios.h>
-#include <unistd.h>
+#include<iostream>
+#include<fstream>
+#include<termios.h>
+#include<unistd.h>
 #include<vector>
 using namespace std;
 void clearScreen(){
@@ -50,6 +51,11 @@ struct fish{
     int max_health=100;
     char symbol='>';
 };
+struct profile{
+    string username;
+    int highscore;
+    int daysSurvived;
+};
 void draw(const fish& fish){
     vector<string>grid(height, string(width, ' '));
     grid[fish.y][fish.x]=fish.symbol;
@@ -76,11 +82,129 @@ void handleinput(char key, fish& fish, bool& running){
         fish.y=ny;
     }
 }
+void welcomeScreen(){
+    clearScreen();
+    cout << "\n\n";
+    cout << "     +=====================================+\n";
+    cout << "     |                                     |\n";
+    cout << "     |                ~~ Prey ~~           |\n";
+    cout << "     |                                     |\n";
+    cout << "     |   A fish alone in a dangerous sea.  |\n";
+    cout << "     |   Survive each day to earn points.  |\n";
+    cout << "     |                                     |\n";
+    cout << "     |   * = Food      (+20 HP)            |\n";
+    cout << "     |   X = Poison    (-15 HP)            |\n";
+    cout << "     |   V = Predator  (danger!)           |\n";
+    cout << "     |                                     |\n";
+    cout << "     |   WASD = Move   Q = Quit            |\n";
+    cout << "     |                                     |\n";
+    cout << "     |      Press ENTER to dive in...      |\n";
+    cout << "     |                                     |\n";
+    cout << "     +=====================================+\n";
+    cout.flush();
+    disableRaw();
+    cin.get();
+    enableRaw();
+}
+profile loadProfiles(const string& username){
+    ifstream file("profiles.txt");
+    string line;
+    while(getline(file, line)){
+        if(line.empty())
+            continue;
+        string storedUn=line.substr(0, line.find(','));
+        if(storedUn==username){
+            profile p;
+            p.username=storedUn;
+            string rest=line.substr(line.find(',')+1);
+            p.highscore=stoi(rest.substr(0, rest.find(',')));
+            p.daysSurvived=stoi(rest.substr(rest.find(',')+1));
+            return p;
+        }
+    }
+    return {"", -1, 0};
+}
+void saveProfiles(const profile& p){
+    ifstream fileIn("profiles.txt");
+    vector<string> lines;
+    string line;
+    while(getline(fileIn, line)){
+        if(!line.empty())
+            lines.push_back(line);
+    }
+    fileIn.close();
+    bool found=false;
+    for(auto& l:lines){
+        string storedun=l.substr(0, l.find(','));
+        if(storedun==p.username){
+            l=p.username+","+to_string(p.highscore)+","+to_string(p.daysSurvived);
+            found=true;
+            break;
+        }
+    }
+    if(!found)
+        lines.push_back(p.username+","+to_string(p.highscore)+","+to_string(p.daysSurvived));
+    ofstream fileOut("profiles.txt");
+    for(const auto& l:lines){
+        fileOut<<l<<"\n";
+    }
+}
+profile profileScreen(){
+    disableRaw();
+    clearScreen();
+
+    cout<<"\n\n";
+    cout<<"+=====================================+\n";
+    cout<<"|                                     |\n";
+    cout<<"|               Prey                  |\n";
+    cout<<"|           Profile Login             |\n";
+    cout<<"|                                     |\n";
+    cout<<"+=====================================+\n\n";
+    cout<<"Enter your username:";
+    cout.flush();
+    string username;
+    cin>>username;
+    profile profiles=loadProfiles(username);
+    if(profiles.highscore==-1){
+        profiles={username, 0, 0};
+        saveProfiles(profiles);
+        clearScreen();
+        cout << "\n\n";
+        cout << "     +=====================================+\n";
+        cout << "     |                                     |\n";
+        cout << "     |  Profile created for " << username << "!\n";
+        cout << "     |  Good luck out there...             |\n";
+        cout << "     |                                     |\n";
+        cout << "     +=====================================+\n\n";
+    }
+    else{
+        clearScreen();
+        cout << "\n\n";
+        cout << "     +=====================================+\n";
+        cout << "     |                                     |\n";
+        cout << "     |  Welcome back, "<<username<<"!\n";
+        cout << "     |                                     |\n";
+        cout << "     |  High Score   : "<<profiles.highscore<<"\n";
+        cout << "     |  Days Survived: "<<profiles.daysSurvived<<"\n";
+        cout << "     |                                     |\n";
+        cout << "     +=====================================+\n\n"; 
+    }
+    cout<<"Press Enter to continue...\n";
+    cin.ignore();
+    cin.get();
+    enableRaw();
+    return profiles;
+}
 int main(){
     enableRaw();
     hideCursor();
+    profile profiles=profileScreen();
+    welcomeScreen();
     fish fish;
     bool running=true;
+    int day=1;
+    int score=0;
+    int daysSurvived=0;
     while(running){
         char key=getKey();
         if(key!=0)
@@ -88,6 +212,10 @@ int main(){
         draw(fish);
         sleepMs(100);
     }
+    if(score>profiles.highscore)
+        profiles.highscore=score;
+    profiles.daysSurvived+=daysSurvived;
+    saveProfiles(profiles);
     showCursor();
     disableRaw();
     return 0;
